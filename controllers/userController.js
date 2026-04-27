5a4bb2ac74e704349ab10b462305ceac116840ae
Theera12 <aarthy.maaya@gmail.com>
2026-04-26 19:29:09 -0700

    Fixed password leak

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,9 @@ const register = (req, res) => {
   const newUser = { ...req.body }; // this makes a copy
   global.users.push(newUser);
   global.user_id = newUser; // After the registration step, the user is set to logged on.
-  delete req.body.password;
-  res.status(StatusCodes.CREATED).json(newUser);
+  const userResponse = { ...newUser };
+  delete userResponse.password;
+  res.status(StatusCodes.CREATED).json(userResponse);
 };
 
 //logging on with the user
