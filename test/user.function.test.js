3fe914fe75925f89ead3a3d2b74daf5e7d35614f
Theera12 <aarthy.maaya@gmail.com>
2026-05-31 17:38:45 -0700

    completed testing

---
+++ test/user.function.test.js
@@ -0,0 +1,85 @@
+require("dotenv").config();
+const request = require("supertest");
+process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+const prisma = require("../db/prisma");
+
+let agent;
+let saveRes;
+
+const { app, server } = require("../app");
+
+beforeAll(async () => {
+  // clear database
+  await prisma.Task.deleteMany(); // delete all tasks
+  await prisma.User.deleteMany(); // delete all users
+  agent = request.agent(app);
+});
+
+describe("register a user ", () => {
+  let saveRes = null; // we'll declare this out here, so that we can reference it in several tests
+  it("46. it creates the user entry", async () => {
+    const newUser = {
+      name: "John Deere",
+      email: "jdeere@example.com",
+      password: "Pa$$word20",
+    };
+    saveRes = await agent.post("/api/users/register").send(newUser);
+    expect(saveRes.status).toBe(201);
+  });
+
+  it("47. Registration returns an object with the expected name.", async () => {
+    const newUser = {
+      name: "Johns Deere",
+      email: "jdeer@example.com",
+      password: "Pa$$word20",
+    };
+    saveRes = await agent.post("/api/users/register").send(newUser);
+    expect(saveRes.body.user.name).toBe("Johns Deere");
+  });
+
+  it("48. Test that the returned object includes a csrfToken.", async () => {
+    const newUser = {
+      name: "Johns",
+      email: "deer@example.com",
+      password: "Pa$$word20",
+    };
+    saveRes = await agent.post("/api/users/register").send(newUser);
+    csrfToken = saveRes.body.csrfToken;
+    expect(saveRes.body.csrfToken).toBeDefined();
+  });
+
+  it("49. You can logon as the newly registered user.", async () => {
+    const newUser = {
+      email: "deer@example.com",
+      password: "Pa$$word20",
+    };
+    loginRes = await agent.post("/api/users/logon").send(newUser);
+    csrfToken = loginRes.body.csrfToken;
+    expect(loginRes.statusCode).toBe(200);
+  });
+
+  it("50. Verify that you are logged in: /api/tasks should not return a 401", async () => {
+    const res = await agent.get("/api/tasks");
+
+    expect(res.statusCode).not.toBe(401);
+  });
+
+  it("51. Verify that you can log out.", async () => {
+    const res = await agent
+      .post("/api/users/logoff")
+      .set("x-csrf-token", csrfToken);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("52. Make sure that you are really logged out: /api/tasks should now return a 401", async () => {
+    const res = await agent.get("/api/tasks");
+
+    expect(res.statusCode).toBe(401);
+  });
+});
+
+afterAll(async () => {
+  prisma.$disconnect();
+  server.close();
+});
